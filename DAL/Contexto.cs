ae6e3a5b7eee8b318b03b96183d781faf1f1a6ef
Junior Emmnauel Baez Tavarez <82235652+JuniorBaez-ux@users.noreply.github.com>
2021-10-05 12:30:30 -0400

    Creando el contexto en conjunto con sus propiedades y metodos.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Junior_20190009.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Junior_20190009.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"data source = Data\aportescontrol.db");
+        }
+    }
+}
