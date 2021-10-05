978216cb935940b4f797fd2e5e01262e9bac034a
Junior Emmnauel Baez Tavarez <82235652+JuniorBaez-ux@users.noreply.github.com>
2021-10-05 12:35:35 -0400

    Creacion de la base de datos, Update de la base de datos y modificacion de la propiedad Copy to output directory a Copy Always.

---
+++ Migrations/20211005163312_Migracion Inicial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P1_AP1_Junior_20190009.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    aporteID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    persona = table.Column<string>(type: "TEXT", nullable: true),
+                    concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    monto = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.aporteID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
